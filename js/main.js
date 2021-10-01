9f33e92c52a6f2135ec9ce6895a049e2f4f2e547
Aviyank Aryan <aviyank123@gmail.com>
2021-10-01 17:53:33 +0530

    js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+import Game from "./Game.js"
+
+
+let game = new Game();
+
+
+console.log("My turn is", game.turn);
+
+//console.log("hello");
---
+++ js/new.js
@@ -0,0 +1,26 @@
+export default class Game {
+
+    constructor(){
+        console.log("init game");
+        this.turn = "X";
+        this.board = new Array(9).fill(null)
+    }
+
+    nextTurn(){
+        if(this.turn == "X") {
+            this.turn = "O";
+        } else {
+            this.turn = "X"
+        }
+    }
+
+    makeMove(i){
+        this.board(i) = this.turn;
+    }
+}
+
+
+let game = new Game();
+
+
+console.log("My turn is", game.turn);
